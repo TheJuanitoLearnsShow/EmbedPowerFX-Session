2d6b57d819fdeb7d712fe526c23e378e0d6dce8f
The Juanito Learns Show <98781316+TheJuanitoLearnsShow@users.noreply.github.com>
2026-04-11 10:07:10 -0400

    Benchmark + cleanup

---
+++ src/PowerFXSamples/0-RuntimeSamples/BenchmarkSample/Program.cs
@@ -0,0 +1,25 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using System.Security.Cryptography;
+
+namespace BenchmarkSample
+{
+    public class PowerFxBecnhmark
+    {
+        [Benchmark]
+        public decimal RunPowerFXFormulas()
+        {
+            var result = SamplePowerFx.GetTestValue();
+            return result;
+        }
+
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<PowerFxBecnhmark>();
+        }
+    }
+}
---
+++ src/PowerFXSamples/0-RuntimeSamples/BenchmarkSample/SamplePowerFx.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.PowerFx;
+using Microsoft.PowerFx.Types;
+
+namespace BenchmarkSample
+{
+    public class SamplePowerFx
+    {
+        public static decimal GetTestValue()
+        {
+            var engine = new RecalcEngine();
+
+            engine.UpdateVariable("EmailTxt", "jj@gamil.com");
+
+            // notice the escaped quotes as we are passing a PowerFX string literal. Our second parameter needs to be 
+            //  a valid PowerFX expression.
+            engine.SetFormula("MyName", "\"Juanito\"", OnFormulaUpdate);
+
+            // engine.SetFormula("MyName", "\"Juanito\"", OnFormulaUpdate); // throws error because MyName is already defined (System.InvalidOperationException: MyName is already defined)
+
+            engine.SetFormula("Greeting", "\"Hello, \" & MyName & \"!\"", OnFormulaUpdate);
+            engine.SetFormula("IsEmail",
+                """
+            If(
+                Find(
+                   "@",
+                   EmailTxt
+                ),
+                true,
+                false
+            )
+            """, OnFormulaUpdate);
+
+            var greetingResult = engine.Eval("Greeting");
+            var isEmailResult = engine.Eval("IsEmail");
+
+            var isEmailResult2 = engine.Eval("IsEmail");
+
+            // 2nd Example
+            engine.UpdateVariable("Velocity", 20);
+            engine.SetFormula("Mass", "2", OnFormulaUpdate);
+            engine.SetFormula("KineticEnergy", "(1/2) * Mass * Velocity * Velocity", OnFormulaUpdate);
+            var energyResult = engine.GetValue("KineticEnergy");
+            return (energyResult as DecimalValue).Value;
+        }
+
+        private static void OnFormulaUpdate(string arg1, FormulaValue value)
+        {
+        }
+    }
+}
